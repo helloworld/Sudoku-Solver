a18105c6812f81a5fbf95c4160ab4e5667d9e0f3
Sashank Thupukari <sthupukari@gmail.com>
2014-11-15 19:04:06 -0500

    Cell Object

--- sudoku.py
+++ sudoku.py
@@ -1,1 +1,11 @@
-print('Initial Commit')
+class Cell(object):
+    """Individual Cells in the Matrix"""
+    def __init__(self, val, r, c, matrix):
+        if (val != 0):
+            self.value = {val, }
+        else:
+            self.value = {1, 2, 3, 4, 5, 6, 7, 8, 9, }
+        self.row = r
+        self.col = c
+        self.block = blockNumber(r, c)
+        self.matrix = matrix
